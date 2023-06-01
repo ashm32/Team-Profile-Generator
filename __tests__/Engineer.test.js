a72a22069f335a99fa8bde3cc62a05343490c5ee
ashm32 <127209262+ashm32@users.noreply.github.com>
2023-06-01 18:10:22 +0100

    Update Engineer.test.js
    comments explain what each case is testing

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,23 @@
+// Import the Engineer class from the appropriate file
+const Engineer = require("../lib/Engineer");
+
+// Test case: Can set GitHub account via constructor
+test("Can set GitHub account via constructor", () => {
+  const testValue = "GitHubUser";
+  const e = new Engineer("Foo", 1, "test@test.com", testValue);
+  expect(e.github).toBe(testValue);
+});
+
+// Test case: getRole() should return "Engineer"
+test("getRole() should return \"Engineer\"", () => {
+  const testValue = "Engineer";
+  const e = new Engineer("Foo", 1, "test@test.com", "GitHubUser");
+  expect(e.getRole()).toBe(testValue);
+});
+
+// Test case: Can get GitHub username via getGithub()
+test("Can get GitHub username via getGithub()", () => {
+  const testValue = "GitHubUser";
+  const e = new Engineer("Foo", 1, "test@test.com", testValue);
+  expect(e.getGithub()).toBe(testValue);
+});
